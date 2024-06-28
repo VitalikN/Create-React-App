833c86db80c5b4d2ae6aaf96b771456bd6673c0d
vitalik.nozhenko@gmail.com <109283146+VitalikN@users.noreply.github.com>
2024-06-28 21:21:25 +0300

    fix

---
+++ NeedHelp/NeedHelp.jsx
@@ -0,0 +1,92 @@
+import React from "react";
+import { usePostCommentMutation } from "../../../redux/helpApi/helpApi";
+
+import * as yup from "yup";
+import { Formik, Form } from "formik";
+import url from "../../../images/icons/sprite/icons.svg";
+import {
+  CloseButton,
+  NeedHelpContainer,
+  Title,
+  Button,
+  Wrapper,
+  StyleErrorMessage,
+  Error,
+  InputComment,
+  InputEmail,
+} from "./NeedHelp.styled";
+
+const schema = yup.object().shape({
+  email: yup
+    .string()
+    .email("Invalid email")
+    .test("email-format", "Invalid email format", (value) => {
+      const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+      return emailRegex.test(value);
+    }),
+  comment: yup.string().required("Comment is required"),
+});
+
+export const NeedHelpModal = ({ handleClose }) => {
+  const [postComment, { isLoading, error }] = usePostCommentMutation();
+
+  const handleSubmit = async (values, { resetForm }) => {
+    try {
+      await postComment(values);
+      console.log(values);
+      resetForm();
+      handleClose();
+    } catch (error) {
+      console.error("Error posting comment:", error);
+    }
+  };
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+  return (
+    <>
+      <NeedHelpContainer>
+        <CloseButton onClick={handleClose}>
+          X
+          <svg width="20" height="20" fill="#ffffff">
+            <use href={`${url}#icon-x-close`} />
+          </svg>
+        </CloseButton>
+        <Title>Need help</Title>
+        <Formik
+          initialValues={{ email: "", comment: "" }}
+          validationSchema={schema}
+          onSubmit={handleSubmit}
+        >
+          <Form>
+            <Wrapper>
+              <InputEmail type="email" name="email" placeholder="Email" />
+              <StyleErrorMessage name="email">
+                {(msg) => <Error>{msg}</Error>}
+              </StyleErrorMessage>
+            </Wrapper>
+
+            <Wrapper>
+              <InputComment
+                name="comment"
+                placeholder="Comment"
+                component="textarea"
+              />
+
+              <StyleErrorMessage name="comment">
+                {(msg) => <Error>{msg}</Error>}
+              </StyleErrorMessage>
+            </Wrapper>
+
+            <Button type="submit">Send</Button>
+          </Form>
+        </Formik>
+      </NeedHelpContainer>
+    </>
+  );
+};
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,14 @@ import React from "react-dom";
 // import { Spinner } from "./spinner/Spinner";
 // import { MainLoader } from "./MainLoader/MainLoader";
 import { Balls } from "./Ball/Ball";
+import { MainLoader } from "./MainLoader/MainLoader";
+import { Spinner } from "./spinner/Spinner";
 
 export const App = () => {
   return (
     <>
-      <Balls />
-      {/* <Spinner /> */}
+      {/* <Balls /> */}
+      <Spinner />
       {/* <MainLoader /> */}
     </>
   );
